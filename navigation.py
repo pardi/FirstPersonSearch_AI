e1deda24ae970dd024089d3d85d57c9d55f12a96
Pardi Tommaso <tommaso.pardi@gmail.com>
2023-07-27 13:33:27 +0100

    feat(): created parsing for the inputs

--- navigation.py
+++ navigation.py
@@ -1,10 +1,11 @@
 import torch
-from DQN_agent import DQNAgent
+from network.network import DQNAgent
 from collections import deque
 from navigationEnv import NavigationEnv
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
+import argparse
 
 def mov_avg(data, window):
     v = deque([] * window)
@@ -18,33 +19,36 @@ def mov_avg(data, window):
     return ma_data
 
 
-def main(train=True):
-    # define the device to run the code into: GPU when available, CPU otherwise
+def run_network(params: dict):
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # define the device to run the code into: GPU when available, CPU otherwise
+    device = torch.device(params["use_gpu"] if torch.cuda.is_available() else "cpu")
 
     file_path = "Banana_Linux/Banana.x86"
-    env = NavigationEnv(file_path)
+    env = NavigationEnv(params["file_env_path"])
 
     # Network parameters
-    gamma = 0.99
-    eps = 1.0
-    eps_min = 5e-2
-    eps_decay = 0.98
+    agent_params = {"gamma": params["gamma"],
+                    "state_size": env.state_size,
+                    "action_size": env.action_size,
+                    "device": device,
+                    "eps": 1.0,
+                    "eps_min": 5e-2,
+                    "eps_decay": 0.98,
+                    "replay_buffer_size": int(1e6),
+                    "batch_size": 128}
 
     # Final score
     final_score = 13.0
 
     # Create agent
-    agent = DQNAgent(gamma, env.state_size, env.action_size, device, eps=eps, eps_min=eps_min, eps_decay=eps_decay)
+    agent = DQNAgent(agent_params)
 
-    best_weight_path = "navigation_weight.pt"
+    best_weight_path = os.path.join(params["best_weight_folder"], "navigation_weight.pt")
 
-    if not train:
+    if not params["train"]:
         agent.load(best_weight_path)
 
-    # Set number of episodes
-    n_episodes = 2000
     # Set timeout
     max_t = 500
 
@@ -53,7 +57,7 @@ def main(train=True):
     # last 100 scores
     scores_window = deque(maxlen=100)
 
-    for i_episode in range(1, n_episodes + 1):
+    for i_episode in range(1, params["num_episodes"] + 1):
 
         state, score = env.reset()
 
@@ -61,7 +65,7 @@ def main(train=True):
             action = agent.egreedy_policy(state)
             next_state, reward, done, _ = env.step(action)
 
-            if train:
+            if params["train"]:
                 agent.step(state, action, reward, next_state, done)
 
             state = next_state
@@ -78,13 +82,12 @@ def main(train=True):
         agent.update_eps()
 
         if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+            print('\rEpisode {i_episode}\tAverage Score: {np.mean(scores_window)}')
 
         # Check if we hit the final score
-        if np.mean(scores_window) >= final_score and train:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window) >= final_score and params["train"]:
+            print(f"\nEnvironment solved in {i_episode} episodes!\tAverage Score: {np.mean(scores_window)}")
             agent.save(best_weight_path)
-            # break
 
     # Average all scores
     window_avg = 100
@@ -97,13 +100,27 @@ def main(train=True):
 
 
 if __name__ == "__main__":
-    # Set train to True to train
 
-    # Set train to False to run the best weight
+    parser = argparse.ArgumentParser(description='This program trains and/or executes a DQN agent to solve the '
+                                                 'Banana search environment.')
 
-    main(train=True)
+    parser.add_argument('--train', action='store_true', help='Run a single episode', choices=[True, False],
+                        default=False)
+    parser.add_argument('--num_episodes', action='store_true', help='Number of episodes to run in training mode',
+                        default=1)
+    parser.add_argument('--best_weight_folder', action='store_true', help='Folder storing the weights for the network',
+                        default="best_weights/")
+    parser.add_argument('--use_gpu', action='store_true', help='Use GPU',  choices=[True, False], default=True)
+    args = parser.parse_args()
 
+    param = {"train": args.train,
+             "num_episodes": args.num_episodes,
+             "best_weight_folder": args.best_weight_folder,
+             "use_gpu": "cuda:0" if args.use_gpu == "True" else "cpu",
+             "gamma": 0.99,
+             "file_env_path": "Banana_Linux/Banana.x86"}
 
+    run_network(param)
 
 
 
